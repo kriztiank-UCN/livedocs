ae817f96106d5c195406cc2ce82792c7742e1f93
Kristian <10221425@ucn.dk>
2024-08-11 12:20:11 +0200

    HomePage

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,50 @@
+import AddDocumentBtn from "@/components/AddDocumentBtn"
+// import { DeleteModal } from '@/components/DeleteModal';
+import Header from "@/components/Header"
+// import Notifications from '@/components/Notifications';
 import { Button } from "@/components/ui/button"
+// import { getDocuments } from '@/lib/actions/room.actions';
+// import { dateConverter } from '@/lib/utils';
+import { SignedIn, UserButton } from "@clerk/nextjs"
+import Image from "next/image"
+import { currentUser } from "@clerk/nextjs/server"
+// import Link from 'next/link';
+import { redirect } from "next/navigation"
 
-const Home = () => {
+const Home = async () => {
+  const clerkUser = await currentUser()
+
+  if (!clerkUser) redirect("/sign-in")
+
+  const documents = []
   return (
-    <div>
-      <Button>Click me</Button>
-    </div>
+    <main className="home-container">
+      <Header className="sticky left-0 top-0">
+        <div className="flex items-center gap-2 lg:gap-4">
+          {/* <Notifications /> */}
+          Notifications
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </div>
+      </Header>
+
+      {documents.length > 0 ? (
+        <div></div>
+      ) : (
+        <div className="document-list-empty">
+          <Image
+            src="/assets/icons/doc.svg"
+            alt="Document"
+            width={40}
+            height={40}
+            className="mx-auto"
+          />
+
+          <AddDocumentBtn userId={clerkUser.id} email={clerkUser.emailAddresses[0].emailAddress} />
+        </div>
+      )}
+    </main>
   )
 }
 export default Home
